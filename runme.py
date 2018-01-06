a06276afdbac025ebd265f91ea98c1d3f1076ada
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2018-01-06 13:41:56 -0700

    Code works as desired.

--- runme.py
+++ runme.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-from userdata import *
 import os
 import subprocess
 
 
 def main():
+    # workdir = '/tftpboot/mitel_patch'
+    workdir = '.'
+    # cfg_file_dir = '/tftpboot'
+    cfg_file_dir = './samplefiles'
+
+    excluded_files_filename = 'excluded_extensions'
+    cfg_files = os.listdir(cfg_file_dir)
+
     excluded_file = os.path.join(workdir, excluded_files_filename)
 
     if not os.path.isfile(excluded_file):
--- userdata.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# workdir = '/tftpboot/mitel_patch'
-workdir = '.'
-#cfg_file_dir = '/tftpboot'
-cfg_file_dir = './samplefiles'
-
-excluded_files_filename = 'excluded_extensions'
-cfg_files = os.listdir(cfg_file_dir)
