06de1fac0aee61b5ab35ffb5cf84da2f7760e3fb
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2018-01-06 14:42:02 -0700

    Changed to running constantly and "sleeping" between cycles.

--- runme.py
+++ runme.py
@@ -2,32 +2,35 @@
 
 import os
 import subprocess
-
+import time
 
 def main():
-    # workdir = '/tftpboot/mitel_patch'
-    workdir = '.'
-    # cfg_file_dir = '/tftpboot'
-    cfg_file_dir = './samplefiles'
+    while True:
+        sleep_time = 300
+        # workdir = '/tftpboot/mitel_patch'
+        workdir = '.'
+        # cfg_file_dir = '/tftpboot'
+        cfg_file_dir = './samplefiles'
 
-    excluded_files_filename = 'excluded_extensions'
-    cfg_files = os.listdir(cfg_file_dir)
+        excluded_files_filename = 'excluded_extensions'
+        cfg_files = os.listdir(cfg_file_dir)
 
-    excluded_file = os.path.join(workdir, excluded_files_filename)
+        excluded_file = os.path.join(workdir, excluded_files_filename)
 
-    if not os.path.isfile(excluded_file):
-        with open(excluded_file, 'w'):
-            pass
-    with open(excluded_file, 'r') as file:
-        excluded_files = file.read().splitlines()
+        if not os.path.isfile(excluded_file):
+            with open(excluded_file, 'w'):
+                pass
+        with open(excluded_file, 'r') as file:
+            excluded_files = file.read().splitlines()
 
-    for file in cfg_files:
-        if 'MN_' in file and file not in excluded_files:
-            print("{} is being modified to fix Mitel model type in the XML.".format(file))
-            true_file = os.path.join(cfg_file_dir, file)
-            subprocess.call(["""sed -i -e 's/Model="5224"/Model="5235"/g' {}""".format(true_file)], shell=True)
-        else:
-            print("{} is being excluded.".format(file))
+        for file in cfg_files:
+            if 'MN_' in file and file not in excluded_files:
+                print("{} is being modified to fix Mitel model type in the XML.".format(file))
+                true_file = os.path.join(cfg_file_dir, file)
+                subprocess.call(["""sed -i -e 's/Model="5224"/Model="5235"/g' {}""".format(true_file)], shell=True)
+            else:
+                print("{} is being excluded.".format(file))
+        time.sleep(sleep_time)
 
 
 if __name__ == "__main__":
