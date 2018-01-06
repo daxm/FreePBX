43c86532e586b44f8f5d402d201683570454a642
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2018-01-06 14:14:30 -0700

    Fixed pathing issue.

--- runme.py
+++ runme.py
@@ -18,14 +18,14 @@ def main():
     if not os.path.isfile(excluded_file):
         with open(excluded_file, 'w'):
             pass
-    with open('excluded_extensions', 'r') as file:
+    with open(excluded_file, 'r') as file:
         excluded_files = file.read().splitlines()
 
     for file in cfg_files:
         if 'MN_' in file and file not in excluded_files:
             print("{} is being modified to fix Mitel model type in the XML.".format(file))
             true_file = os.path.join(cfg_file_dir, file)
-            subprocess.call(["""sed -i -e 's/Model="5235"/Model="daxm"/g' {}""".format(true_file)], shell=True)
+            subprocess.call(["""sed -i -e 's/Model="5224"/Model="5235"/g' {}""".format(true_file)], shell=True)
         else:
             print("{} is being excluded.".format(file))
 
