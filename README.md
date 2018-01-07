ebbd7fe237190a05a0c11c3c6491b4d7cf232ad7
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2018-01-06 22:03:24 -0700

    Added README.md and systemd Unit file.

---
+++ README.md
@@ -0,0 +1,17 @@
+This is a simple/crappy way of fixing FreePBX to allow the use of Mitel 5235 phones.  The issue is that, for some
+unknown reason, FreePBX does not have a template for the Mitel 5235 phones.  However, their cfg file is identical
+to the 5224 model.  Knowing that allows us to "patch" the cfg files and rename their "Paramater Model=" XML setting to
+"5235" instead of "5224".
+
+Each time FreePBX modifies those files (say, adding a button to the phone) it will overwrite our change, so this
+program will periodically scan the **/tftpboot** folder and look for cfg files that need re-patched.
+
+In order to NOT change the cfg files of Mitel 5224 phones (if you have some) there is an **excluded_extensions** file
+that you can put the filenames of the cfg files you want this program to skip.  Just put one filename per line.
+
+The guy whom I wrote this script for wanted this program to reside in /tftpboot/mitel_patch folder.  You may want to
+store this file somewhere else.  If so, modify the **workdir** variable in the script to point to your desired location.
+
+Additionally, I created the systemd service file that you can use to run this script at bootup of the FreePBX server.
+Just copy the **mitel_patch.service** file to **/etc/systemd/system/** folder.  Then issue the command
+**systemctl enable mitel_patch.service** to configure this file to run at system startup.
--- runme.py
+++ runme.py
@@ -1,20 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import os
 import subprocess
 import time
+import logging
+
 
 def main():
+    logging.basicConfig(level='DEBUG')
+    
     while True:
         sleep_time = 300
-        # workdir = '/tftpboot/mitel_patch'
-        workdir = '.'
-        # cfg_file_dir = '/tftpboot'
-        cfg_file_dir = './samplefiles'
 
-        excluded_files_filename = 'excluded_extensions'
+        workdir = '/tftpboot/mitel_patch'
+        # workdir = '/home/daxm/PycharmProjects/FreePBX'
+
+        cfg_file_dir = '/tftpboot'
+        # cfg_file_dir = os.path.join(workdir, 'samplefiles')
         cfg_files = os.listdir(cfg_file_dir)
 
+        excluded_files_filename = 'excluded_extensions'
         excluded_file = os.path.join(workdir, excluded_files_filename)
 
         if not os.path.isfile(excluded_file):
@@ -25,11 +30,12 @@ def main():
 
         for file in cfg_files:
             if 'MN_' in file and file not in excluded_files:
-                print("{} is being modified to fix Mitel model type in the XML.".format(file))
+                logging.info("{} is being modified to fix Mitel model type in the XML.".format(file))
                 true_file = os.path.join(cfg_file_dir, file)
                 subprocess.call(["""sed -i -e 's/Model="5224"/Model="5235"/g' {}""".format(true_file)], shell=True)
             else:
-                print("{} is being excluded.".format(file))
+                logging.info("{} is being excluded.".format(file))
+        logging.info("Sleeping for {} seconds.".format(sleep_time))
         time.sleep(sleep_time)
 
 
